cbf3c6854615dd340a5f52b56310b2a6f3b7988d
GracefulTing <32625852+GracefulTing@users.noreply.github.com>
2018-04-19 14:14:30 +0800

    Add files via upload

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about container">
+	<h1 class="page-header">希望能够提供给大家更有用的课程!</h1>
+	<p>如果大家需要代码，可以加QQ群！</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
