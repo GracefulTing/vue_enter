854f803c46812035312f0f5609ff8ef874c42a9c
GracefulTing <32625852+GracefulTing@users.noreply.github.com>
2018-04-20 09:25:12 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two> -->
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+export default {
+  name: 'app',
+  components:{
+    'product-list-one':ProductListOne,
+    'product-list-two':ProductListTwo
+  },
+  data () {
+    return {
+      // products:[
+      //   {name:"马云",price:200},
+      //   {name:"马化腾",price:140},
+      //   {name:"马冬梅",price:20},
+      //   {name:"马蓉",price:10},
+      // ]
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  font-family: Ubuntu;
+  color:#555;
+}
+</style>
